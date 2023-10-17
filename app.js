df440df7920e89900d004e94edf0d551f9b33031
EmanueleVenditti95 <vendittiema@gmail.com>
2023-10-17 14:27:09 +0200

    Add - pseudocode

---
+++ app.js
@@ -0,0 +1,15 @@
+// Visualizzare in pagina 5 numeri casuali:
+// - definire una funzione per generare 5 numeri casuali dentro un Array
+// - stampare i numeri in pagina
+
+// Timer di 30 secondi in cui i numeri non devono essere visibili:
+// - far scomparire i numeri
+// - far partire un timer di 30 secondi
+
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt():
+// - definire una funzione che,tramite prompt, chiede un numero all'utente
+// - ripeterla x 5 volte
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati:
+// - controllare se e quanti numeri,di quelli inseriti dall'utente, siano presenti nell'array di numeri casuali
+// - stampare il punteggio
