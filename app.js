da1363cd1c4d6d7e71215583288c43f48d1fe7ea
EmanueleVenditti95 <vendittiema@gmail.com>
2023-10-17 15:49:13 +0200

    Fix - set timeout instead of interval, (cicle not workin)

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ console.log(arrayRandomNumbers)
 // - stampare i numeri in pagina tramite un alert
 alertMessage("I tuoi numeri sono" + ' ' + arrayRandomNumbers + '.Ricordali!');
 
-// Timer di 30 secondi in cui i numeri non devono essere visibili:
-// - far scomparire i numeri
-// - far partire un timer di 30 secondi
-let seconds = 30
-const idInterval = setInterval(timer, 1000);
-function timer() {
-    seconds--
-    console.log(seconds)
-    if (seconds <= 0) {clearInterval(idInterval)};
-}
-
-
-
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt():
-// - definire una funzione che,tramite prompt, chiede un numero all'utente
-// - ripeterla x 5 volte
+// - far partire un timer di 30 secondi
+// - scaduto il timer, definire una ciclo per chiedere all'utente 5 numeri tramite prompt e inserirli in un array
+let arrayUserNumbers = []
+setTimeout(getArrayOfUserNumbers, 5000);
+    function getArrayOfUserNumbers () {
+    const arrayUserNumbers = [];
+    while (arrayRandomNumbers.length < 5) {
+        let userNumber = prompt('Inserisci numero');
+        arrayUserNumbers.push(userNumber);
+    }
+    return arrayUserNumbers;
+    }
+console.log(arrayUserNumbers)
+
+
+
 
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati:
 // - controllare se e quanti numeri,di quelli inseriti dall'utente, siano presenti nell'array di numeri casuali
@@ -29,7 +30,6 @@ function timer() {
 
 
 function getArrayOfRandomNumbers(minRange, maxRange, number) {
-
     const arrayRandomNumbers = [];
     while (arrayRandomNumbers.length < number) {
         let n = getRandomInt(minRange, maxRange);
